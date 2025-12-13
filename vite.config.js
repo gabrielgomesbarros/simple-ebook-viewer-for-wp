aa12cebf5f79104be1151f6594cc329ec14a8ded
Francesco Martini <wrCisco@users.noreply.github.com>
2025-12-13 23:01:36 +0100

    onfigure Vite to create a bundle usable both as library and as a script
    Needs a bit of tidying up

--- vite.config.js
+++ vite.config.js
@@ -2,18 +2,23 @@
 import { defineConfig } from 'vite'
 import { v4wp } from '@kucrut/vite-for-wp';
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
+import libAssetsPlugin from '@laynezh/vite-plugin-lib-assets'
 
 export default defineConfig({
     plugins: [
         v4wp( {
             input: {
                 'simebv-viewer-lib': 'src/js/simebv-viewer.js',
+                'simebv-viewer-init': 'src/js/simebv-init.js',
             },
             outDir: 'dist',
         } ),
         nodePolyfills({
             include: ['fs', 'http', 'https'],
         }),
+        // libAssetsPlugin({
+        //     include: [/\.pfb$/, /\.bcmap$/, /\.map$/, /\.mjs$/, /\.woff2$/, /\.ttf$/],
+        // }),
     ],
     server: {
         cors: {
@@ -25,13 +30,41 @@ export default defineConfig({
         origin: 'http://www.embedebooks-wp.it',
     },
     esbuild: {
-        supported: {
-            'top-level-await': true,
-        },
+        // supported: {
+        //     'top-level-await': true,
+        // },
+        minifyIdentifiers: false,
+        keepNames: true,
     },
     build: {
+        // lib: {
+        //     entry: {'simebv-viewer-lib': 'src/js/simebv-viewer.js'},
+        //     formats: ['es'],
+        // },
         minify: true,
-        sourceMap: false,
-        manifest: true
+        // terserOptions: {
+        //     mangle: {
+        //         reserved: ["Reader", "initializeViewer"],
+        //         keep_classnames: /Reader/,
+        //         keep_fnames: /initializeViewer/,
+        //         properties: {
+        //             reserved: ["Reader", "initializeViewer", "__", "_x", "_n", "sprintf", "source_url"],
+        //         },
+        //     },
+        //     // compress: {
+        //     //     keep_classnames: /Reader/,
+        //     //     keep_fnames: /initializeViewer/,
+        //     // },
+        // },
+        sourcemap: false,
+        manifest: true,
+        chunkSizeWarningLimit: 610,
+        rollupOptions: {
+            output: {
+                entryFileNames: "[name].js",
+                // exports: "named",
+            },
+            preserveEntrySignatures: "strict",
+        },
     },
 });
