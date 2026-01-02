42c3365a61f73a69447ffb05f516649a5d3d902b
Francesco Martini <wrCisco@users.noreply.github.com>
2026-01-02 03:39:12 +0100

    Remove unused parts of vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@
 import { defineConfig } from 'vite'
 import { v4wp } from '@kucrut/vite-for-wp';
 import { nodePolyfills } from 'vite-plugin-node-polyfills'
-import libAssetsPlugin from '@laynezh/vite-plugin-lib-assets'
 
 export default defineConfig({
     plugins: [
@@ -16,9 +15,6 @@ export default defineConfig({
         nodePolyfills({
             include: ['fs', 'http', 'https'],
         }),
-        // libAssetsPlugin({
-        //     include: [/\.pfb$/, /\.bcmap$/, /\.map$/, /\.mjs$/, /\.woff2$/, /\.ttf$/],
-        // }),
     ],
     server: {
         cors: {
@@ -30,39 +26,17 @@ export default defineConfig({
         origin: 'http://www.embedebooks-wp.it',
     },
     esbuild: {
-        // supported: {
-        //     'top-level-await': true,
-        // },
         minifyIdentifiers: false,
         keepNames: true,
     },
     build: {
-        // lib: {
-        //     entry: {'simebv-viewer-lib': 'src/js/simebv-viewer.js'},
-        //     formats: ['es'],
-        // },
         minify: true,
-        // terserOptions: {
-        //     mangle: {
-        //         reserved: ["Reader", "initializeViewer"],
-        //         keep_classnames: /Reader/,
-        //         keep_fnames: /initializeViewer/,
-        //         properties: {
-        //             reserved: ["Reader", "initializeViewer", "__", "_x", "_n", "sprintf", "source_url"],
-        //         },
-        //     },
-        //     // compress: {
-        //     //     keep_classnames: /Reader/,
-        //     //     keep_fnames: /initializeViewer/,
-        //     // },
-        // },
         sourcemap: false,
         manifest: true,
         chunkSizeWarningLimit: 610,
         rollupOptions: {
             output: {
                 entryFileNames: "[name].js",
-                // exports: "named",
             },
             preserveEntrySignatures: "strict",
         },
